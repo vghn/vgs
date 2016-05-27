be6e335b9ad508580f2d52c44541c25ec5c528cc
Vlad <vlad@ghn.me>
2016-05-27 09:57:14 -0500

    Improve function arguments

--- lib/misc.sh
+++ lib/misc.sh
@@ -10,13 +10,11 @@
 #   3) The text message (required)
 vgs_notify_slack(){
   local channel username text
-  channel="${1:?}"
-  username="${2:?}"
-  text="${3:?}"
+  channel="${1:?Must specify the channel name as the 1st argument}"
+  username="${2:?Must specify the user name as the 2nd argument}"
+  text="${3:?Must specify the message as the 3rd argument}"
 
-  # red:   ce0814
-  # green: 3ca553
-  # blue:  4484c2
+  # Colors: red: ce0814, green: 3ca553, blue: 4484c2
 
   printf "Notify slack... "
   curl -s -X POST --data-urlencode "payload={\"channel\": \"#${channel}\", \"username\": \"${username}\", \"text\": \"${text}\", \"icon_emoji\": \":taurus:\"}" "$SLACK_WEBHOOK"
@@ -31,9 +29,9 @@ vgs_notify_slack(){
 #   3) The encrypted file (required)
 vgs_encrypt(){
   local key in out
-  key="${1:?}"
-  in="${2:?}"
-  out="${3:?}"
+  key="${1:?Must specify the encryption key as the 1st argument}"
+  in="${2:?Must specify the decrypted file as the 2nd argument}"
+  out="${3:?Must specify the encrypted file as the 3rd argument}"
 
   # Start a subshell and ensure it is not run in debug mode
   ( set +e; openssl aes-256-cbc -k "$key" -in "$in" -out "$out" )
@@ -48,9 +46,9 @@ vgs_encrypt(){
 #   3) The decrypted file (required)
 vgs_decrypt(){
   local key in out
-  key="${1:?}"
-  in="${2:?}"
-  out="${3:?}"
+  key="${1:?Must specify the encryption key as the 1st argument}"
+  in="${2:?Must specify the decrypted file as the 2nd argument}"
+  out="${3:?Must specify the encrypted file as the 3rd argument}"
 
   # Start a subshell and ensure it is not run in debug mode
   ( set +e; openssl aes-256-cbc -k "$key" -in "$in" -out "$out" -d )
