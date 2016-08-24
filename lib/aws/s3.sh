089eb61a9c67a880b08e84af7f9fd8588e4de224
Vlad <vlad@ghn.me>
2016-08-24 17:40:02 -0500

    The AWS-CLI now has a presign command

--- lib/aws/s3.sh
+++ lib/aws/s3.sh
@@ -1,23 +1,6 @@
 #!/usr/bin/env bash
 # AWS S3 functions
 
-# NAME: vgs_aws_s3_generate_presigned_url
-# DESCRIPTION: Returns a temporary presigned url for an existing S3 object.
-# USAGE: vgs_aws_s3_generate_presigned_url {Bucket} {Key} {Expiration}
-#        Download with wget -O myFile "https://..." (Note the quotes)
-# PARAMETERS:
-#   1) The bucket name (required)
-#   2) The key name (required)
-#   3) The expiration time, in seconds (defaults: 600)
-vgs_aws_s3_generate_presigned_url(){
-  local bucket key expires
-  bucket=${1:?Must specify the S3 bucket name as the 1st argument}
-  key=${2:?Must specify the S3 key name as the 2nd argument}
-  expires=${3:-600}
-
-  python <<< "import boto3; print boto3.client('s3').generate_presigned_url(ClientMethod='get_object', Params={'Bucket': '${bucket}', 'Key': '${key}'}, ExpiresIn = ${expires})"
-}
-
 # NAME: vgs_aws_s3_list_old_keys
 # DESCRIPTION: Returns a list of S3 objects, sorted by last modified date.
 #              The newest X items are excluded.
